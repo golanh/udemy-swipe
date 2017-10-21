613388dabe8556254b2258731029795186c61489
golanh <golanhuppert9@gmail.com>
2017-10-21 16:47:28 +0300

    move & rotate the Card

--- src/Deck.js
+++ src/Deck.js
@@ -7,26 +7,49 @@ class Deck extends Component {
     constructor(props) {
         super(props);
 
+        this.position = new Animated.ValueXY();
         this.panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, gesture) => {
-                // console.log(gesture);
+                this.position.setValue({ x: gesture.dx });
             },
-            onPanResponderRelease: () => {},
+            onPanResponderRelease: () => { },
+        });
+    }
+
+    getCardStyle() {
+        const rotate = this.position.x.interpolate({
+            inputRange: [-500, 0, 500],
+            outputRange: ['-120deg', '0deg', '120deg']
         });
 
+        return {
+            ...this.position.getLayout(),
+            transform: [{ rotate }]
+        };
     }
 
     renderCards() {
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
             return this.props.renderCard(item);
         });
     }
-    
+
     render() {
         return (
-            <View {...this.panResponder.panHandlers}>
-            {this.renderCards()}
+            <View>
+                {this.renderCards()}
             </View>
         );
     }
